9bd8939f1b44f5d8501d91cd804febdffbf8de0d
furkanmavili <furkanmavili@outlook.com>
2020-07-26 18:25:07 +0300

    Add test

---
+++ src/App.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { act, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import App from "./App";
+
+test("clock header functionality", () => {
+  act(() => {
+    render(<App />);
+  });
+  expect(screen.getByText("25:00")).toBeInTheDocument();
+  act(() => {
+    userEvent.click(screen.getByRole("button", { name: /Short Break/ }));
+  });
+  expect(screen.getByText("05:00")).toBeInTheDocument();
+  act(() => {
+    userEvent.click(screen.getByRole("button", { name: /Long Break/ }));
+  });
+  expect(screen.getByText("15:00")).toBeInTheDocument();
+});
