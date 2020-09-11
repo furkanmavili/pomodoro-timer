31f0acd5089b8c2442bdd98ee1ac900b5c2736d1
furkanmavili <furkanmavili@outlook.com>
2020-09-11 16:06:54 +0300

    add mobile home screen

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Reading from "./images/ReadingDoodle.svg";
 import { Grid, Hidden } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import classnames from "classnames";
+import { useEffect } from "react";
 
 const initialSettings = {
   sound: true,
@@ -51,6 +52,37 @@ function App() {
   const [activeMode, setActiveMode] = useState("pomodoro");
   const [settings, setSettings] = useState(initialSettings);
   const classes = useStyles();
+  let deferredPrompt;
+  const addBtn = document.querySelector(".add-button");
+  addBtn.style.display = "none";
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI to notify the user they can add to home screen
+      addBtn.style.display = "block";
+
+      addBtn.addEventListener("click", (e) => {
+        // hide our user interface that shows our A2HS button
+        addBtn.style.display = "none";
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === "accepted") {
+            console.log("User accepted the A2HS prompt");
+          } else {
+            console.log("User dismissed the A2HS prompt");
+          }
+          deferredPrompt = null;
+        });
+      });
+    });
+    return () => window.removeEventListener("beforeinstallprompt");
+  }, []);
+
   return (
     <Grid
       container
@@ -100,6 +132,7 @@ function App() {
       <div className={classes.social}>
         <Social />
       </div>
+      <button class=".add-button">Add to home screen</button>
     </Grid>
   );
 }
