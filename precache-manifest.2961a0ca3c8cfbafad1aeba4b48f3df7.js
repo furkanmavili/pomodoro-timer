0eb8ef727470cf3b2c00645b65abcf686f78f92b
furkanmavili <furkanmavili@outlook.com>
2020-09-11 16:08:03 +0300

    Updates

---
+++ precache-manifest.2961a0ca3c8cfbafad1aeba4b48f3df7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "054367b561432ce9b1b12c876bbd9eb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b14b028622614709c5b",
+    "url": "/static/css/main.a1b94e98.chunk.css"
+  },
+  {
+    "revision": "7c630f62adb6be1222a3",
+    "url": "/static/js/2.427f12f9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.427f12f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4125a8202d3bdf2bbedb",
+    "url": "/static/js/3.c3f51b58.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/static/js/3.c3f51b58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b14b028622614709c5b",
+    "url": "/static/js/main.ae24c6eb.chunk.js"
+  },
+  {
+    "revision": "e005b868bb4e131534c1",
+    "url": "/static/js/runtime-main.c87a97a2.js"
+  },
+  {
+    "revision": "98b197c51a838defa428e977f8cb4a84",
+    "url": "/static/media/DancingDoodle.98b197c5.svg"
+  },
+  {
+    "revision": "160c4c55a25910b7a9a5ee048c5eb8c3",
+    "url": "/static/media/ReadingDoodle.160c4c55.svg"
+  },
+  {
+    "revision": "10737d2884c45a81d519a79b7cb2e83a",
+    "url": "/static/media/bell.10737d28.mp3"
+  },
+  {
+    "revision": "c9fa9aa78955ed8f5ef7e79ff459b3ec",
+    "url": "/static/media/button.c9fa9aa7.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pomodoro-timer/precache-manifest.8a4a142caed8395cc205ab5dd7342346.js"
+  "/precache-manifest.2961a0ca3c8cfbafad1aeba4b48f3df7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pomodoro-timer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
