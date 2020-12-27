fa0a1f83f65b5b7f908174070627589b402ec4d5
furkanmavili <furkanmavili@outlook.com>
2020-12-27 12:48:32 +0300

    Remove old 'Add to homescreen' button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import ClockPanel from "./components/ClockPanel";
 import Social from "./components/Social";
@@ -8,7 +8,6 @@ import Reading from "./images/ReadingDoodle.svg";
 import { Grid, Hidden } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import classnames from "classnames";
-import { useEffect } from "react";
 
 const initialSettings = {
   sound: true,
@@ -52,45 +51,9 @@ function App() {
   const [activeMode, setActiveMode] = useState("pomodoro");
   const [settings, setSettings] = useState(initialSettings);
   const classes = useStyles();
-  const addBtn = useRef();
-  addBtn.current.style.display = "none";
-
-  useEffect(() => {
-    let deferredPrompt;
-    window.addEventListener("beforeinstallprompt", (e) => {
-      // Prevent Chrome 67 and earlier from automatically showing the prompt
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      deferredPrompt = e;
-      // Update UI to notify the user they can add to home screen
-      addBtn.current.style.display = "block";
-
-      addBtn.current.addEventListener("click", (e) => {
-        // hide our user interface that shows our A2HS button
-        addBtn.current.style.display = "none";
-        // Show the prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === "accepted") {
-            console.log("User accepted the A2HS prompt");
-          } else {
-            console.log("User dismissed the A2HS prompt");
-          }
-          deferredPrompt = null;
-        });
-      });
-    });
-    return () => window.removeEventListener("beforeinstallprompt");
-  }, []);
 
   return (
-    <Grid
-      container
-      className={classes.root}
-      alignContent="flex-start"
-      justify="center"
-    >
+    <Grid container className={classes.root} alignContent="flex-start" justify="center">
       <Grid xs={12} item align="center" className={classes.header}>
         <Header settings={settings} setSettings={setSettings} />
       </Grid>
@@ -98,29 +61,19 @@ function App() {
         <Hidden smDown>
           <Grid item md={4}>
             <img
-              className={classnames(
-                classes.image,
-                activeMode === "pomodoro" ? "" : classes.passive
-              )}
+              className={classnames(classes.image, activeMode === "pomodoro" ? "" : classes.passive)}
               src={Reading}
               alt="reading doogle"
             />
           </Grid>
         </Hidden>
         <Grid item xs={10} sm={8} md={4}>
-          <ClockPanel
-            settings={settings}
-            activeMode={activeMode}
-            changeMode={setActiveMode}
-          />
+          <ClockPanel settings={settings} activeMode={activeMode} changeMode={setActiveMode} />
         </Grid>
         <Hidden smDown>
           <Grid item sm={0} md={4}>
             <img
-              className={classnames(
-                classes.image,
-                activeMode === "pomodoro" ? classes.passive : ""
-              )}
+              className={classnames(classes.image, activeMode === "pomodoro" ? classes.passive : "")}
               src={DancingDoodle}
               alt="dancing doogle"
             />
@@ -133,9 +86,6 @@ function App() {
       <div className={classes.social}>
         <Social />
       </div>
-      <button ref={addBtn} className=".add-button">
-        Add to home screen
-      </button>
     </Grid>
   );
 }
