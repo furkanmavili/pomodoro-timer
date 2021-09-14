d3b97b8179ffb695d72f7737b62f5f2b39fdc2ad
furkanmavili <furkanmavili@outlook.com>
2021-09-14 21:41:35 +0300

    format file

--- src/components/Social.js
+++ src/components/Social.js
@@ -2,29 +2,22 @@ import React from "react";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import { ButtonGroup, IconButton } from "@material-ui/core";
+
 function Social() {
-  return (
-    <ButtonGroup>
-      <IconButton>
-        <a
-          href="https://www.github.com/furkanmavili"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <GitHubIcon />
-        </a>
-      </IconButton>
-      <IconButton>
-        <a
-          href="https://www.twitter.com/ffmavili"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <TwitterIcon />
-        </a>
-      </IconButton>
-    </ButtonGroup>
-  );
+    return (
+        <ButtonGroup>
+            <IconButton>
+                <a href="https://www.github.com/furkanmavili" target="_blank" rel="noopener noreferrer">
+                    <GitHubIcon />
+                </a>
+            </IconButton>
+            <IconButton>
+                <a href="https://www.twitter.com/ffmavili" target="_blank" rel="noopener noreferrer">
+                    <TwitterIcon />
+                </a>
+            </IconButton>
+        </ButtonGroup>
+    );
 }
 
 export default Social;
