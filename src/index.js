c48f15f09b1483d87805ea4e1ce2151e74e6a721
furkanmavili <furkanmavili@outlook.com>
2021-09-19 14:43:22 +0300

    remove theme provider

--- src/index.js
+++ src/index.js
@@ -2,42 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#679b9b",
-    },
-    secondary: {
-      main: "#f7c5a8",
-    },
-  },
-  typography: {
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
-  },
-});
-theme.typography.h1 = {
-  fontSize: "5rem",
-
-  [theme.breakpoints.up("md")]: {
-    fontSize: "6rem",
-  },
-};
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
+    <App />,
   document.getElementById("root")
 );
