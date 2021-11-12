54630a403bec6113f99048df0fbf13a2426b49b5
furkanmavili <furkanmavili@outlook.com>
2021-11-12 23:27:33 +0300

    reformat file

--- src/App.test.js
+++ src/App.test.js
@@ -5,16 +5,16 @@ import "@testing-library/jest-dom";
 import App from "./App";
 
 test("clock header functionality", () => {
-  act(() => {
-    render(<App />);
-  });
-  expect(screen.getByText("25:00")).toBeInTheDocument();
-  act(() => {
-    userEvent.click(screen.getByRole("button", { name: /Short Break/ }));
-  });
-  expect(screen.getByText("05:00")).toBeInTheDocument();
-  act(() => {
-    userEvent.click(screen.getByRole("button", { name: /Long Break/ }));
-  });
-  expect(screen.getByText("15:00")).toBeInTheDocument();
+    act(() => {
+        render(<App />);
+    });
+    expect(screen.getByText("25:00")).toBeInTheDocument();
+    act(() => {
+        userEvent.click(screen.getByRole("button", { name: /Short Break/ }));
+    });
+    expect(screen.getByText("05:00")).toBeInTheDocument();
+    act(() => {
+        userEvent.click(screen.getByRole("button", { name: /Long Break/ }));
+    });
+    expect(screen.getByText("15:00")).toBeInTheDocument();
 });
