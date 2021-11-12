33822e72a92b2b495a377ba65f643c592959022e
furkanmavili <furkanmavili@outlook.com>
2021-11-12 23:31:03 +0300

    remove unneccesary components from home component

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,17 +1,11 @@
 import React from "react";
 import { Container } from "@mui/material";
-// import Header from "../../components/Header";
-import ClockPanel from "../../components/ClockPanel";
-import Social from "../../components/Social";
-import Settings from "../../components/Settings";
+import ClockPanel from "../../components/ClockPanel/";
 
 function Home() {
     return (
         <Container maxWidth="sm" sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-            {/* <Header /> */}
-            <Settings />
             <ClockPanel />
-            <Social />
         </Container>
     );
 }
